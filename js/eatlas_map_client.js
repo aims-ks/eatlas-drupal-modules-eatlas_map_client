088c11b768ae90740377fc19fa0b94bb87b43f02
Marc Hammerton <m.hammerton@aims.gov.au>
2018-01-31 16:09:02 +1000

    parse map banner URL and use data to display layers

--- js/eatlas_map_client.js
+++ js/eatlas_map_client.js
@@ -1,18 +1,37 @@
 (function ($) {
-	// Execute when the page is ready
-	$(document).ready(function(){
-		console.log('Loading eAtlas Map Clients');
 
-		$('.eatlas-map-client-map').each(function() {
-			var mapClient = new aimsMap.MapClient($(this).attr('id'), {
-				projection: 'EPSG:4326',
-				mapConfigHost: $(this).data('map-config-host'),
-				mapConfigURL: $(this).data('map-config-url')
-			});
-			mapClient.init();
+	$(document).ready(function () {
+		console.log('Loading eAtlas Map Client');
+
+		var mapClientDiv = $('.eatlas-map-client-map')[0];
+		var mapConfig = $(mapClientDiv).data('map-config');
+
+		var mapClient = new aimsMap.MapClient($(mapClientDiv).attr('id'), {
+			projection: 'EPSG:4326',
+			centre: mapConfig.centre,
+			zoom: mapConfig.zoom,
+			mapConfigHost: mapConfig.configHost,
+			mapConfigURL: mapConfig.configUrl
 		});
 
-		console.log('eAtlas Map Clients successfully loaded');
-	});
+		mapClient.init()
+			.then(function () {
+				var baseLayersAPI = mapClient.getBaseLayersAPI();
+				mapConfig.layers.forEach(function(layer) {
+					baseLayersAPI.addAvailableLayer(layer.id);
+					if (layer.visible) {
+						baseLayersAPI.addActiveLayer(layer.id);
+					}
+				});
 
+				var overlayLayersAPI = mapClient.getOverlayLayersAPI();
+				mapConfig.layers.forEach(function(layer) {
+					overlayLayersAPI.addAvailableLayer(layer.id);
+					if (layer.visible) {
+						overlayLayersAPI.addActiveLayer(layer.id);
+					}
+				});
+				console.log('eAtlas Map Clients successfully loaded');
+		});
+	})
 })(jQuery);
